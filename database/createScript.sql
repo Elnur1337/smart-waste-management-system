31618eeb93aaa7bc548ad88e0eb37534fed4bdd6
Elnur Bjelić <elnur.bjelic1337@gmail.com>
2024-01-06 17:04:54 +0100

    Renamed location name to address and changed max.. size to 100

--- database/createScript.sql
+++ database/createScript.sql
@@ -4,7 +4,7 @@ CREATE TABLE locations (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	lat DECIMAL(9, 6) NOT NULL,
     lng DECIMAL(9, 6) NOT NULL,
-    locationName VARCHAR(25) NOT NULL UNIQUE,
+    address VARCHAR(100) NOT NULL UNIQUE,
     locationDescription VARCHAR(100)
 );
 
