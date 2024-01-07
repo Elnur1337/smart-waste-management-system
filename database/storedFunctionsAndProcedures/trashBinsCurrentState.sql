a670a798d10d53c85fc039b31e5d1c6fd225a825
Elnur Bjelić <elnur.bjelic1337@gmail.com>
2024-01-07 18:51:09 +0100

    Created stored procedure that return current state of all trash bins

---
+++ database/storedFunctionsAndProcedures/trashBinsCurrentState.sql
@@ -0,0 +1,48 @@
+DELIMITER //
+CREATE PROCEDURE trashBinsCurrentState()
+BEGIN
+    CREATE TEMPORARY TABLE trashBinsLastPickup AS
+    SELECT trashBinId, dateAndTime
+    FROM trashBinsInPickups
+    JOIN pickups ON trashBinsInPickups.pickupId = pickups.id
+    WHERE pickups.dateAndTime =
+    (
+		SELECT MAX(dateAndTime) FROM pickups
+        JOIN trashBinsInPickups trashBinsInPickupsN ON trashBinsInPickupsN.pickupId = pickups.id
+        WHERE trashBinsInPickupsN.trashBinId = trashBinsInPickups.trashBinId
+	);
+    
+	CREATE TEMPORARY TABLE lastUpdatesTime AS
+	SELECT
+		trashBinId,
+		MAX(dateAndTime) AS dateAndTime
+	FROM updates
+	GROUP BY trashBinId;
+    
+    CREATE TEMPORARY TABLE lastUpdates AS
+	SELECT
+		DISTINCT updates.dateAndTime,
+		updates.trashBinId,
+		updates.fillLevel
+	FROM updates
+	JOIN lastUpdatesTime ON updates.trashBinId = lastUpdatesTime.trashBinId AND updates.dateAndTime = lastUpdatesTime.dateAndTime;
+    
+    SELECT
+		trashBins.id,
+        locations.lat,
+        locations.lng,
+        locations.address,
+        lastUpdates.fillLevel,
+        trashBinsLastPickup.dateAndTime,
+        IF(lastUpdates.fillLevel >= 75 OR trashBinsLastPickup.dateAndTime < DATE_ADD(NOW(), INTERVAL -2 DAY), "Y", "N") AS readyForPickup,
+        lastUpdates.dateAndTime AS lastUpdate,
+        IF(lastUpdates.dateAndTime < DATE_ADD(NOW(), INTERVAL -2 DAY), "Y", "N") AS possibleBreak
+	FROM trashBins
+    JOIN locations ON trashBins.locationId = locations.id
+    JOIN lastUpdates ON trashBins.id = lastUpdates.trashBinid
+    LEFT JOIN trashBinsLastPickup ON trashBins.id = trashBinsLastPickup.trashBinId;
+    
+    DROP TABLE trashBinsLastPickup;
+	DROP TABLE lastUpdatesTime;
+	DROP TABLE lastUpdates; 
+END//
