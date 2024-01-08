19cbc1b9488d6e181a590a0310b63a8676529955
Elnur Bjelić <elnur.bjelic1337@gmail.com>
2024-01-08 13:25:09 +0100

    Created SQL update script to update locations to local locations

---
+++ database/updateLocations.sql
@@ -0,0 +1,72 @@
+UPDATE locations
+SET
+	lat = 44.535983,
+    lng = 18.655633,
+    address = "Irac 1, Tuzla"
+WHERE id = 1;
+UPDATE locations
+SET
+	lat = 44.535733,
+    lng = 18.655013,
+    address = "Irac 2, Tuzla"
+WHERE id = 2;
+UPDATE locations
+SET
+	lat = 44.531509,
+    lng = 18.650902,
+    address = "BCC Tuzla"
+WHERE id = 3;
+UPDATE locations
+SET
+	lat = 44.536059,
+    lng = 18.662544,
+    address = "Mosnik 1, Tuzla"
+WHERE id = 4;
+UPDATE locations
+SET
+	lat = 44.537571,
+    lng = 18.662875,
+    address = "Mejdan 1, Tuzla"
+WHERE id = 5;
+UPDATE locations
+SET
+	lat = 44.537371,
+    lng = 18.663752,
+    address = "Mejdan 2, Tuzla"
+WHERE id = 6;
+UPDATE locations
+SET
+	lat = 44.538340,
+    lng = 18.662672,
+    address = "Omega, Tuzla"
+WHERE id = 7;
+UPDATE locations
+SET
+	lat = 44.541563,
+    lng = 18.665332,
+    address = "Slatina 1, Tuzla"
+WHERE id = 8;
+UPDATE locations
+SET
+	lat = 44.540856,
+    lng = 18.664074,
+    address = "Elektrotenička škola, Tuzla"
+WHERE id = 9;
+UPDATE locations
+SET
+	lat = 44.542094,
+    lng = 18.664876,
+    address = "Slatina 2, Tuzla"
+WHERE id = 10;
+UPDATE locations
+SET
+	lat = 44.530571,
+    lng = 18.688883,
+    address = "Stupine 1, Tuzla"
+WHERE id = 11;
+UPDATE locations
+SET
+	lat = 44.530037,
+    lng = 18.691754,
+    address = "Stupine 2, Tuzla"
+WHERE id = 12;
